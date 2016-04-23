6cdd82838c5317c0de53205c977dec054a1f0b8c
rg3915 <rg3915@yahoo.com.br>
2016-04-23 13:35:00 -0300

    Refactor Mixins

--- dress/core/mixins.py
+++ dress/core/mixins.py
@@ -12,3 +12,27 @@ class NameSearchMixin(object):
                 Q(last_name__icontains=q) |
                 Q(email__icontains=q))
         return queryset
+
+
+class DressSearchMixin(object):
+
+    def get_queryset(self):
+        queryset = super(DressSearchMixin, self).get_queryset()
+        q = self.request.GET.get('search_box')
+        if q:
+            return queryset.filter(
+                Q(dress_model__icontains=q) |
+                Q(stylist__icontains=q))
+        return queryset
+
+
+class OrderSearchMixin(object):
+
+    def get_queryset(self):
+        queryset = super(OrderSearchMixin, self).get_queryset()
+        q = self.request.GET.get('search_box')
+        if q:
+            return queryset.filter(
+                Q(customer__first_name__icontains=q) |
+                Q(dress__dress_model__icontains=q))
+        return queryset
--- dress/core/views.py
+++ dress/core/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy as r
 from django.views.generic import CreateView, ListView, DetailView
 from django.views.generic import UpdateView, DeleteView
-from django.db.models import Q
-from .mixins import NameSearchMixin
+from .mixins import NameSearchMixin, DressSearchMixin, OrderSearchMixin
 from .models import Customer, Dress, Order
 from .forms import CustomerForm, DressForm, OrderForm
 
@@ -27,16 +26,9 @@ customer_delete = DeleteView.as_view(
     model=Customer, success_url=r('core:customer_list'))
 
 
-class DressList(ListView):
+class DressList(DressSearchMixin, ListView):
     model = Dress
     paginate_by = 5
-
-    def get_queryset(self):
-        queryset = super(DressList, self).get_queryset()
-        q = self.request.GET.get('search_box')
-        if q:
-            return queryset.filter(dress_model__icontains=q)
-        return queryset
 
 
 dress_detail = DetailView.as_view(model=Dress)
@@ -49,19 +41,9 @@ dress_delete = DeleteView.as_view(
     model=Dress, success_url=r('core:dress_list'))
 
 
-class OrderList(ListView):
+class OrderList(OrderSearchMixin, ListView):
     model = Order
     paginate_by = 10
-
-    def get_queryset(self):
-        queryset = super(OrderList, self).get_queryset()
-        q = self.request.GET.get('search_box')
-        if q:
-            return queryset.filter(
-                Q(customer__icontains=q) |
-                Q(dress__icontains=q) |
-                Q(price__icontains=q))
-        return queryset
 
 
 order_detail = DetailView.as_view(model=Order)
